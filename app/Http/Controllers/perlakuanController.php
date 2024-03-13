466be9b17ca89a6a52afcaf58e0c15b4893eba6a
fuadadhim24 <fuadadhim24@gmail.com>
2024-03-13 13:21:41 +0700

    feat: add perlakuan page and perlakuanController

---
+++ app/Http/Controllers/perlakuanController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class perlakuanController extends Controller
+{
+    function index(){
+        return view('admin.fase.perlakuan.index');
+    }
+    function tambah(){
+        
+    }
+    function edit(){
+        
+    }
+    function hapus(){
+        
+    }
+}
