0b27b1ecd91b89bff1cce0d5971a8248f9e3dd38
ardianti04 <ardiantisimanjuntak5@gmail.com>
2024-03-27 23:04:05 +0700

    Add show data on view varietas padi

--- app/Http/Controllers/varietasPadiController.php
+++ app/Http/Controllers/varietasPadiController.php
@@ -2,14 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\VarietasPadi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-class varietasPadiController extends Controller
+class VarietasPadiController extends Controller
 {
-    function index() {
-        return view('admin.varietasPadi.index');
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // $padi=VarietasPadi::all();
+        $padi=DB::table('padi')->get();
+        // $padi = VarietasPadi::with('padi')->get();
+        // return $varietas;
+        return view("admin/varietasPadi/index",compact("padi"));
     }
-    function tambahView() {
-        return view('admin.varietasPadi.tambah');
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VarietasPadi $varietasPadi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VarietasPadi $varietasPadi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, VarietasPadi $varietasPadi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VarietasPadi $varietasPadi)
+    {
+        //
     }
 }
